8697c1d67a3d544f55a848f8064862f82cc16983
Tyler Haun <me@tylerhaun.com>
2019-01-23 22:34:04 -0800

    fixed linear scaler

--- src/utils/linear-scaler.js
+++ src/utils/linear-scaler.js
@@ -7,10 +7,10 @@ class LinearScaler {
     }
 
     scale(value) {
-        var xMin = this._inputRange[0];
-        var xMax = this._inputRange[1];
-        var yMin = this._outputRange[0];
-        var yMax = this._outputRange[1];
+        var xMin = this._outputRange[0];
+        var xMax = this._outputRange[1];
+        var yMin = this._inputRange[0];
+        var yMax = this._inputRange[1];
         var percent = (value - yMin) / (yMax - yMin);
         var outputX = percent * (xMax - xMin) + xMin;
         return outputX;
